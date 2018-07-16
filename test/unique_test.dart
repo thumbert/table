fcf78a8f0dc45d4775992b9913d55c0e8c38aa21
thumbert <humbert.tony@yahoo.com>
2018-07-16 10:36:09 -0400

    Create unique_test.dart

---
+++ test/unique_test.dart
@@ -0,0 +1,36 @@
+
+import 'package:test/test.dart';
+import 'package:table/table.dart';
+
+uniqueMapTest() {
+  group('unique maps:', (){
+    List<Map> xs = [
+      {'type': 'A', 'time': new DateTime(2018), 'value': 10},
+      {'type': 'A', 'time': new DateTime(2018), 'value': 10},
+      {'type': 'B', 'time': new DateTime(2018), 'value': 10},
+    ];
+    test('no names', (){
+      var res = unique(xs);
+      expect(res.length, 2);
+    });
+    test('with one name', (){
+      var res = unique(xs, names: ['type']);
+      expect(res.length, 2);
+      expect(res, [{'type': 'A'}, {'type': 'B'},]);
+    });
+    test('with two names', (){
+      xs.add({'type': 'A', 'time': new DateTime(2019), 'value': 10});
+      var res = unique(xs, names: ['type', 'time']);
+      var out = [
+        {'type': 'A', 'time': new DateTime(2018)},
+        {'type': 'B', 'time': new DateTime(2018)},
+        {'type': 'A', 'time': new DateTime(2019)},];
+      expect(res.length, 3);
+      expect(res, out);
+    });
+  });
+}
+
+main() {
+  uniqueMapTest();
+}
