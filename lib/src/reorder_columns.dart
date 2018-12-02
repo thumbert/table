5dd28507450b1fe0d7c9411f66128bfa8413c017
thumberts <tony.humberts@gmail.com>
2018-12-02 13:55:05 -0500

    reorder columns

---
+++ lib/src/reorder_columns.dart
@@ -0,0 +1,18 @@
+library reorder_columns;
+
+/// Return a new collection of rows which contains only the columns
+/// specified in [columnNames].  If a column doesn't exist throw an error.
+List<Map<String, dynamic>> reorderColumns(
+    List<Map<String, dynamic>> xs, List columnNames) {
+  var out = <Map<String, dynamic>>[];
+  for (var x in xs) {
+    var row = <String, dynamic>{};
+    for (var name in columnNames) {
+      if (!x.containsKey(name))
+        throw ArgumentError('Column $name doesn\'t exist in $x');
+      row[name] = x[name];
+    }
+    out.add(row);
+  }
+  return out;
+}
---
+++ test/reorder_columns_test.dart
@@ -0,0 +1,30 @@
+library reorder_columns_test;
+
+import 'package:test/test.dart';
+import 'package:table/src/reorder_columns.dart';
+
+tests() {
+  group('Reorder columns', (){
+    var xs = <Map<String,dynamic>>[
+      {'year': 2016, 'tMin': 22.1, 'tMax': 98.9},
+      {'year': 2018, 'tMin': 21.8, 'tMax': 111.3},
+      {'year': 2019, 'tMin': 20.3, 'tMax': 108.9},
+    ];
+    test('simple reordering', (){
+      var res = reorderColumns(xs, ['year', 'tMax', 'tMin']);
+      expect(res.first.keys.toList(), ['year', 'tMax', 'tMin']);
+    });
+    test('reordering with fewer columns', (){
+      var res = reorderColumns(xs, ['year', 'tMax']);
+      expect(res.first.keys.toList(), ['year', 'tMax']);
+    });
+    test('reordering with a new column throws', (){
+      var res = reorderColumns(xs, ['year', 'tMax', 'wMax']);
+      expect(res.first.keys.toList(), ['year', 'tMax']);
+    });
+  });
+}
+
+main() {
+  tests();
+}
