249258e65ea3ae1c4bb03ff08cfd8e0bc37d273b
thumbert <tony.humberts@gmail.com>
2018-12-04 14:01:04 -0500

    Update table.dart

--- lib/table.dart
+++ lib/table.dart
@@ -4,7 +4,9 @@
 /// The table library.
 library table;
 
+export 'src/add_missing.dart';
 export 'src/flattenMap.dart';
 export 'src/nest.dart';
+export 'src/reshape.dart';
 export 'src/table_base.dart';
 export 'src/unique.dart';
