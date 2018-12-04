c6ff6e0fe32e712c5e7c885f53ea5cf95a094e49
thumbert <tony.humberts@gmail.com>
2018-12-04 14:08:35 -0500

    Update all_test.dart

--- test/all_test.dart
+++ test/all_test.dart
@@ -2,10 +2,12 @@ library all_test;
 
 import 'flatten_test.dart' as flatten;
 import 'nest_test.dart' as nest;
+import 'reshape_test.dart' as reshape;
 import 'table_test.dart' as table;
 
 main() {
   flatten.main();
   nest.main();
+  reshape.tests();
   table.main();
 }
