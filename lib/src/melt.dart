28e9e3e5fbea37ffec7752298e7f75506d5108c5
thumbert <tony.humberts@gmail.com>
2019-12-12 14:05:02 -0500

    Update melt.dart

--- lib/src/melt.dart
+++ lib/src/melt.dart
@@ -1,16 +1,15 @@
 library melt.dart;
 
-/// R's melt for Dart.
 List<Map<String,dynamic>> melt(List<Map<String,dynamic>> xs,
     Set<String> tagsKey, Set<String> variablesKey, {String value: 'value'}) {
   var aux = <Map<String,dynamic>>[];
   for (var x in xs) {
-    var one = Map.fromEntries(x.entries.where((e) => tagsKey.contains(e.key)));
     for (var key in variablesKey) {
+      var one = Map.fromEntries(x.entries.where((e) => tagsKey.contains(e.key)));
       one['variable'] = key;
       one[value] = x[key];
+      aux.add(one);
     }
-    aux.add(one);
   }
   return aux;
 }
