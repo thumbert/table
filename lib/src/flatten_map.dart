210666cdc62866d63af723e2dfe59a27aef46623
thumberts <tony.humberts@gmail.com>
2020-03-16 08:15:32 -0400

    improve types flattenMap

--- lib/src/flatten_map.dart
+++ lib/src/flatten_map.dart
@@ -3,7 +3,7 @@ library flatten_map;
 /// Given a multiple level nested Map, extract the values of the last level.
 /// This function is useful to extract the results of a nest computation. 
 List extractValues(Map x) {
-  List out = [];
+  var out = [];
   if (x.values.first is List) {
     out.addAll(x.values);
   } else {
@@ -34,26 +34,25 @@ List extractValues(Map x) {
 ///
 /// To unwrap only part of the nested map, specify fewer
 /// [levelNames]. 
-List<Map> flattenMap(Map m, List<String> levelNames) {
+List<Map<String,dynamic>> flattenMap(Map m, List<String> levelNames) {
   if (levelNames.isEmpty) return [m];
   return _flatten(m, 0, levelNames);
 }
 
-List<Map> _flatten(Map m, int level, List levelNames) {
-  List<Map> out;
+List<Map<String,dynamic>> _flatten(Map m, int level, List<String> levelNames) {
+  List<Map<String,dynamic>> out;
   var name = levelNames[level];
 
   m.forEach((k, v) {
     if (level < levelNames.length-1 && v is Map) {
-      out ??= <Map>[];
+      out ??= <Map<String,dynamic>>[];
       var aux = _flatten(v, level + 1, levelNames);
       out.addAll(aux.map((Map e) => {name: k}..addAll(e)));
     } else {
       /// last level
-      out ??= <Map>[];
+      out ??= <Map<String,dynamic>>[];
       if (v is Map) {
         if (level == levelNames.length-1) {
-          var _bux = {name: k}..addAll(v);
           out.add({name: k}..addAll(v));
         } else {
           v[name] = k;
